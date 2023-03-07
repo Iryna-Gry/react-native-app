65847e260b31d315c22b41d22a5a87eb866c8271
Iryna Grytsaenko <iryna.grytsaenko@gmail.com>
2023-03-07 18:50:55 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  TouchableWithoutFeedback,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  Alert,
+  Button,
+} from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <TextInput placeholder="Username" style={styles.input} />
+
+      <Button title={"Login"} style={styles.input} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#ecf0f1",
+  },
+  input: {
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "black",
+    marginBottom: 10,
+  },
+});
